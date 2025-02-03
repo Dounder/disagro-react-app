ce0ae8cff74aa98a725b4314c40bb231483b07dd
glasdou.ml <dr.glasdou@gmail.com>
2025-02-03 16:19:42 -0600

    Refactor SelectionCardHeader: optimize search handling with useCallback, streamline input change logic, and enhance search button functionality

--- src/routes/asistencias/[component]/SelectionCardHeader.tsx
+++ src/routes/asistencias/[component]/SelectionCardHeader.tsx
@@ -1,5 +1,5 @@
 import Search from '@/icons/Search';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 interface Prop {
   onSearch: (search: string) => void;
@@ -8,20 +8,20 @@ interface Prop {
 export default function SelectionCardHeader({ onSearch }: Prop) {
   const [search, setSearch] = useState('');
 
-  const handleChange = () => {
-    onSearch(search);
+  const handleSearch = useCallback((query: string) => onSearch(query), [onSearch]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearch(value);
+    handleSearch(value);
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      e.preventDefault(); // Prevent the default form submission
-      onSearch(search);
+      e.preventDefault();
+      handleSearch(search);
     }
   };
-
-  useEffect(() => {
-    onSearch(search);
-  }, [search]);
 
   return (
     <section className="bg-primary w-full h-[15%] flex justify-center items-center px-12">
@@ -31,12 +31,12 @@ export default function SelectionCardHeader({ onSearch }: Prop) {
           className="border-[2px] border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full py-4 px-6"
           placeholder="Buscar Servicios y Productos"
           value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={handleChange}
           onKeyDown={handleKeyDown}
         />
         <button
           type="button"
-          onClick={handleChange}
+          onClick={() => handleSearch(search)}
           className="absolute right-0 top-0 bottom-0 flex items-center justify-center p-4 rounded-full hover:bg-primary/20 transition-all duration-300 ease-in-out">
           <Search />
         </button>
