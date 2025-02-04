b3efe892a83f81d252caf8ec0f77e8aea62178c9
glasdou.ml <dr.glasdou@gmail.com>
2025-02-03 19:02:51 -0600

    Add Docker configuration files and update API image reference; enhance README with Docker usage instructions

--- src/api/api.config.ts
+++ src/api/api.config.ts
@@ -1,8 +1,8 @@
-import { useAuthStore } from '@/routes/auth/[store]/auth.store';
+import { envs } from '@/config/envs';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: envs.apiUrl,
 });
 
 api.interceptors.request.use((config) => {
